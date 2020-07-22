75cda7a956670753f41f3b174836310dcaf01091
Josh Bernfeld <joshbernfeld@gmail.com>
2020-07-21 20:50:40 -0700

    Migrate to CAMetalLayer from MTKView

--- framework/Source/RenderView.swift
+++ framework/Source/RenderView.swift
@@ -7,7 +7,7 @@ public protocol RenderViewDelegate: class {
     func didDisplayTexture(renderView: RenderView, texture: Texture)
 }
 
-public class RenderView: MTKView, ImageConsumer {
+public class RenderView: UIView, ImageConsumer {
     public weak var renderViewDelegate:RenderViewDelegate?
     
     public let sources = SourceContainer()
@@ -15,67 +15,124 @@ public class RenderView: MTKView, ImageConsumer {
     var currentTexture: Texture?
     var renderPipelineState:MTLRenderPipelineState!
     
-    public override init(frame frameRect: CGRect, device: MTLDevice?) {
-        super.init(frame: frameRect, device: sharedMetalRenderingDevice.device)
+    // Separate var so we can access from place other than main thread
+    unowned var internalLayer: CALayer!
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
         
         commonInit()
     }
     
-    public required init(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         super.init(coder: coder)
         
         commonInit()
     }
     
     private func commonInit() {
-        framebufferOnly = false
-        autoResizeDrawable = true // Not sure why but this is necessary for simulator
+        self.internalLayer = self.layer
         
-        self.device = sharedMetalRenderingDevice.device
+        #if targetEnvironment(simulator)
+        if #available(iOS 13.0, *) {
+            let layer = self.internalLayer as! CAMetalLayer
+            layer.framebufferOnly = false
+            layer.device = sharedMetalRenderingDevice.device
+        }
+        #else
+        let layer = self.internalLayer as! CAMetalLayer
+        layer.framebufferOnly = false
+        layer.device = sharedMetalRenderingDevice.device
+        #endif
         
         let (pipelineState, _, _) = generateRenderPipelineState(device:sharedMetalRenderingDevice, vertexFunctionName:"oneInputVertex", fragmentFunctionName:"passthroughFragment", operationName:"RenderView")
         self.renderPipelineState = pipelineState
-        
-        enableSetNeedsDisplay = false
-        isPaused = true
+    }
+    
+    // We use CAMetalLayer instead of MTKView because on iOS 11 we were getting the following crash when moving videos around in the cropper: [CAMetalLayerDrawable texture] should not be called after already presenting this drawable. Get a nextDrawable instead.
+    // This was likely a bug in iOS https://developer.apple.com/forums/thread/64889
+    // We should be using the CAMetalLayer anyways because we don't need the added functionality of MTKView.
+    // MTKView also wouldn't work correctly in simulator unless autoresizeDrawable was set to true which was odd.
+    // Unfortunately we have to jump through a lot of hoops because CAMetalLayer is only available in iOS 13 simulator but all iOS versions for devices.
+    public override class var layerClass : AnyClass {
+        #if targetEnvironment(simulator)
+        if #available(iOS 13.0, *) {
+            return CAMetalLayer.self
+        } else {
+            return CALayer.self
+        }
+        #else
+        return CAMetalLayer.self
+        #endif
     }
     
     public func newTextureAvailable(_ texture:Texture, fromSourceIndex:UInt) {
-        self.drawableSize = CGSize(width: texture.texture.width, height: texture.texture.height)
-        currentTexture = texture
-        self.draw()
+        #if targetEnvironment(simulator)
+        if #available(iOS 13.0, *) {
+            let layer = self.internalLayer as! CAMetalLayer
+            layer.drawableSize = CGSize(width: texture.texture.width, height: texture.texture.height)
+        }
+        #else
+        let layer = self.internalLayer as! CAMetalLayer
+        layer.drawableSize = CGSize(width: texture.texture.width, height: texture.texture.height)
+        #endif
+        self.currentTexture = texture
+        self.render()
     }
     
-    public override func draw(_ rect:CGRect) {
+    public override func draw(_ rect: CGRect) {
+        self.render()
+    }
+    
+    public override func draw(_ layer: CALayer, in ctx: CGContext) {
+        self.render()
+    }
+    
+    func render() {
         // Necessary according to https://developer.apple.com/documentation/quartzcore/cametallayer
         // Without this we were getting a deadlock when setting drawableSize
         autoreleasepool {
-            guard let commandBuffer = sharedMetalRenderingDevice.commandQueue.makeCommandBuffer() else {
-                Log.warning("Could not make command buffer")
+            #if targetEnvironment(simulator)
+            if #available(iOS 13.0, *) {
+                let layer = self.internalLayer as! CAMetalLayer
+                guard let nextDrawable = layer.nextDrawable() else {
+                    Log.warning("Could not retrieve next drawable")
+                    return
+                }
+                renderInternal(nextDrawable: nextDrawable)
+            }
+            #else
+            let layer = self.internalLayer as! CAMetalLayer
+            guard let nextDrawable = layer.nextDrawable() else {
+                Log.warning("Could not retrieve next drawable")
                 return
             }
-            
-            guard let currentDrawable = self.currentDrawable else {
-                Log.warning("Could not retrieve current drawable")
-                return
-            }
-            
-            guard let currentTexture = self.currentTexture else {
-                Log.warning("Could not retrieve current texture")
-                return
-            }
-            
-            self.renderViewDelegate?.willDisplayTexture(renderView: self, texture: currentTexture)
-            
-            let outputTexture = Texture(orientation: .portrait, texture: currentDrawable.texture)
-            commandBuffer.renderQuad(pipelineState: renderPipelineState, inputTextures: [0:currentTexture], outputTexture: outputTexture)
-            
-            commandBuffer.present(currentDrawable)
-            commandBuffer.addCompletedHandler({ (commandBuffer) in
-                self.renderViewDelegate?.didDisplayTexture(renderView: self, texture: currentTexture)
-            })
-            commandBuffer.commit()
+            renderInternal(nextDrawable: nextDrawable)
+            #endif
         }
+    }
+    
+    func renderInternal(nextDrawable: CAMetalDrawable) {
+        guard let commandBuffer = sharedMetalRenderingDevice.commandQueue.makeCommandBuffer() else {
+            Log.warning("Could not make command buffer")
+            return
+        }
+        
+        guard let currentTexture = self.currentTexture else {
+            Log.warning("Could not retrieve current texture")
+            return
+        }
+        
+        self.renderViewDelegate?.willDisplayTexture(renderView: self, texture: currentTexture)
+        
+        let outputTexture = Texture(orientation: .portrait, texture: nextDrawable.texture)
+        commandBuffer.renderQuad(pipelineState: renderPipelineState, inputTextures: [0:currentTexture], outputTexture: outputTexture)
+        
+        commandBuffer.present(nextDrawable)
+        commandBuffer.addCompletedHandler({ (commandBuffer) in
+            self.renderViewDelegate?.didDisplayTexture(renderView: self, texture: currentTexture)
+        })
+        commandBuffer.commit()
     }
 }
 
